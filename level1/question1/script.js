b383d75a9529fef72cde06f5c973020eb4c56fff
Sondre Nordjore <sonoskole@outlook.com>
2019-10-25 09:15:29 +0200

    Finished Product
    Done

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,22 @@
-//Use RegEx to validate form
+// Phone Verification: 
+
+function checkNumber(numberInput) {
+    var regPhone = /^\d{8}$/;
+    if (numberInput.value.match(regPhone)) {
+        console.log(numberInput.value + ' Is a valid number.');
+    } else {
+        console.log(numberInput.value + ', Not a valid number.');
+    }
+};
+
+
+// E-mail verification:
+
+function checkMail(emailInput) {
+    var regMail = /^[a-zA-Z0-9-_.]+@[a-zA-Z-.]+\.[a-zA-Z]{2,5}$/;
+    if (emailInput.value.match(regMail)) {
+        console.log(emailInput.value + 'Is a valid email');
+    } else {
+        console.log(emailInput.value + 'Is not a valid email')
+    }
+};
--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,25 @@
 //Console.log out elements in JSON file
+
+fetch('./author.json')
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+
+        data.video.forEach((obj, i) => {
+            console.log("Id:", obj.id);
+            console.log("Name:", obj.name);
+            console.log("Url:", obj.url);
+            console.log("Author:", obj.author);
+
+        })
+        data.video[0].author.data.forEach((obj, i) => {
+            console.log("Author Name:", obj.name_author);
+            console.log("Url:", obj.url);
+            console.log("Type:", obj.type);
+        })
+    })
+
+    .catch(err => {
+        console.log("error");
+    })
